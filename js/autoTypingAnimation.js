c911491b8f12cb10c22a2a1695e4a010d90709ee
Ved Tiwari <ved@webkorps.com>
2022-12-22 03:34:36 +0530

    Portfolio

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Ved Tiwari</span> i am from jabalpur Currently i am working in WEBKORPS OPTIMAL SOLUTION INDORE as associate softwere engineer."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
