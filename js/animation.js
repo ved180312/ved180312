1f5a9782ffe5f9a0c86042de14201cf35ee52c84
Ved Tiwari <ved@webkorps.com>
2023-02-11 02:34:11 +0530

    made some changes

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Associate Softwere Engineer"];
+const textArray = ["Associate software Engineer"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Ved Tiwari</span> i am from jabalpur Currently i am working in WEBKORPS OPTIMAL SOLUTION INDORE as associate softwere engineer."
+        "<span>Hi, I am Ved Tiwari</span> i am from jabalpur Currently i am working in WEBKORPS OPTIMAL SOLUTION INDORE as associate software engineer."
       ],
       typeSpeed: 7,
       contentType: 'html'
